c7fe16c489aafe8740d1b2c1e2cc9854d2d115e5
Jackie <jackiesalasdev@gmail.com>
2021-09-16 17:30:10 +0800

    udemy homework done

---
+++ analyzer.rb
@@ -0,0 +1,6 @@
+puts "Enter your first name"
+firstName = gets.chomp
+puts " i got it! you are #{firstName}"
+puts "how about your last name?"
+lastName = gets.chomp
+puts "welcome to my mini program #{firstName + " " + lastName}  "
